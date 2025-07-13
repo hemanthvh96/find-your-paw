807795e45755456115b77664b32bdf433a97da15
Hemanth <hemanthvh96@gmail.com>
2025-07-13 09:41:37 -0500

    resolve stale auth issue

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -5,4 +5,18 @@ const apiClient = axios.create({
   withCredentials: true,
 });
 
+apiClient.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.log("Session expired. Logging out");
+      localStorage.removeItem("user");
+      sessionStorage.clear();
+
+      window.location.assign("/find-your-paw/login");
+      return Promise.reject(error);
+    }
+  }
+);
+
 export default apiClient;
